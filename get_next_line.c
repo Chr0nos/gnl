b7e3fc3a0c489cc0646d0a475fadd95cc9741c57
Chr0nos <snicolet@student.42.fr>
2015-12-09 19:52:55 +0100

    joyeu noel

---
+++ get_next_line.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/04 10:11:09 by snicolet          #+#    #+#             */
+/*   Updated: 2015/12/09 19:52:03 by snicolet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include "libft.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+static void		add_pending(char *buffer, t_gnls *x)
+{
+	char	*tmp;
+
+	if (x->pb)
+	{
+		tmp = x->pb;
+		x->pb = ft_strjoin(x->pb, buffer);
+		free(tmp);
+	}
+	else
+		x->pb = ft_strdup(buffer);
+}
+
+static void		rotate_pending(char **pending, size_t offset, int rest_len)
+{
+	char	*tmp;
+
+	tmp = NULL;
+	if (rest_len <= 0)
+	{
+		free(*pending);
+		*pending = NULL;
+	}
+	else if (!(tmp = ft_strdup(*pending + offset)))
+		return ;
+	if (*pending)
+		free(*pending);
+	*pending = tmp;
+}
+
+static int		ft_read_data(char *buffer, t_gnls *x)
+{
+	int			read_lenght;
+	int			rest_lenght;
+
+	add_pending(buffer, x);
+	if (x->pb == NULL)
+		return (-1);
+	read_lenght = ft_strchrpos(x->pb, '\n');
+	rest_lenght = 0;
+	if (read_lenght >= 0)
+	{
+		rest_lenght = ft_strlen(x->pb) - read_lenght;
+		x->buffer = ft_strndup(x->pb, read_lenght);
+		x->buffer[read_lenght] = '\0';
+		rotate_pending(&x->pb, read_lenght + 1, rest_lenght);
+		return (1);
+	}
+	return (0);
+}
+
+static int		ft_gnl_read(const int fd, t_gnls *x)
+{
+	char	buffer[BUFF_SIZE + 1];
+	int		ret;
+	int		ret_b;
+
+	x->buffer = 0;
+	ret = 1;
+	while ((ret = read(fd, buffer, BUFF_SIZE)))
+	{
+		if (ret < 0)
+			return (ret);
+		buffer[ret] = '\0';
+		ret_b = ft_read_data(buffer, x);
+		if (ret_b == 1)
+			return (1);
+	}
+	buffer[0] = '\0';
+	while ((ret_b = ft_read_data(buffer, x)))
+	{
+		if (ret_b < 0)
+			return (ret_b);
+		if (ret_b == 1)
+			return (1);
+	}
+	return (0);
+}
+
+int				get_next_line(int const fd, char **line)
+{
+	static t_gnls	x;
+	int				ret;
+
+	*line = 0;
+	ret = ft_gnl_read(fd, &x);
+	if (ret >= 0)
+		*line = x.buffer;
+	return (ret);
+}
---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_next_line.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/05 14:15:48 by snicolet          #+#    #+#             */
+/*   Updated: 2015/12/09 16:42:12 by snicolet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_GET_NEXT_LINE_H
+# define FT_GET_NEXT_LINE_H
+# define BUFF_SIZE 999
+# include <string.h>
+
+typedef struct	s_gnlstatic_of_doom
+{
+	char		*buffer;
+	char		*pb;
+}				t_gnls;
+
+int				get_next_line(int const fd, char **line);
+
+#endif
