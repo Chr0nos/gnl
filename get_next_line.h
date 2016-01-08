fc34ec9aa1fb933ffa0286aa5f3919c41f5ddc13
Chr0nos <snicolet@student.42.fr>
2016-01-08 16:05:04 +0100

    fix pour rendu

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,13 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/05 14:15:48 by snicolet          #+#    #+#             */
-/*   Updated: 2015/12/13 15:12:30 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/01/08 16:00:56 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_GET_NEXT_LINE_H
 # define FT_GET_NEXT_LINE_H
 # define BUFF_SIZE 999
-# include <string.h>
 
 typedef struct	s_gnlstatic_of_doom
 {
